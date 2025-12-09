e43f668d688af10cbd151bf2cefa9293742ba441
Shreya Madeshiya <smadeshiya12345@gmail.com>
2025-12-10 00:05:06 +0530

    app.js  path chnaged due to deployement issue

---
+++ app.js
@@ -0,0 +1,38 @@
+const express=require('express');
+const connectionDB=require("./src/Config/database")
+const User=require("./src/models/user");
+const {ValidationSignupData}=require('./src/utils/validation');
+const bcrypt = require("bcryptjs");
+const app=express();
+const cookieParser=require('cookie-parser');
+const jwt=require('jsonwebtoken');
+const { auth } = require('./src/middlewares/auth');
+const authRouter=require('./src/routes/auth');
+const requestRouter=require('./src/routes/request');
+const profileRouter=require('./src/routes/profile');
+const userRouter=require('./src/routes/user');
+require('dotenv').config();
+const cors=require('cors');
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173", // Your React app's URL
+  credentials: true, // Allow cookies or Authorization headers
+}));
+
+app.use("/",authRouter);
+app.use("/",requestRouter);
+app.use("/",profileRouter);
+app.use("/",userRouter);
+
+ connectionDB().then(()=>{
+    console.log("DB Connection created successfully");
+    app.listen(process.env.PORT,()=>{
+    console.log("server Created successfully at  port 4000");
+});
+}).catch(err=>{
+    console.error("there is some error in connecting to DB:",err);
+});
+
+
+
--- src/Config/database.js
+++ src/Config/database.js
@@ -1,5 +1,7 @@
  const mongoose=require('mongoose');
+ require('dotenv').config();
  const connectionDB=async()=>{
-    await mongoose.connect("mongodb+srv://shreya19457:KwzezBL2Exo2JyD2@cluster0.nxabzdq.mongodb.net/devTinder");
+    await mongoose.connect(process.env.DB_CONNECTION_SECRET);
  }
 module.exports=connectionDB;
+
